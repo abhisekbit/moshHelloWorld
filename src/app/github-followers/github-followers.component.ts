92906894a4638b39fbb89676fdafcb992579359e
Abhisek Das <abhisek@Abhiseks-MacBook-Pro.local>
2018-05-02 00:39:14 +0530

    Routing done

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,7 +1,14 @@
+import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute } from '@angular/router';
 import { GithubFollowersService } from './../services/github-followers.service';
 import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/switchMap'
+
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'github-followers',
   templateUrl: './github-followers.component.html',
   styleUrls: ['./github-followers.component.css']
@@ -9,10 +16,23 @@ import { Component, OnInit } from '@angular/core';
 export class GithubFollowersComponent implements OnInit {
   followers: any[];
 
-  constructor(private service: GithubFollowersService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: GithubFollowersService) { }
 
   ngOnInit() {
-    this.service.getAll()
-      .subscribe(followers => this.followers = followers);
+    // tslint:disable-next-line:prefer-const
+    Observable.combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+    ])
+    .switchMap(combined => {
+      // tslint:disable-next-line:prefer-const
+      let id = combined[0].get('id');
+      // tslint:disable-next-line:prefer-const
+      let paged = combined[1].get('page');
+      return this.service.getAll();
+    })
+    .subscribe(followers => this.followers = followers);
   }
 }
--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-github-profile',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute) { }
+
+  submit() {
+    this.router.navigate(['/followers'], {
+      queryParams: { page: 1, order: 'newest' }
+    });
+  }
 
   ngOnInit() {
+    // tslint:disable-next-line:prefer-const
+    let id = this.route.snapshot.paramMap.get('id');
+    console.log(id);
+
+    // tslint:disable-next-line:prefer-const
+    let username = this.route.snapshot.paramMap.get('username');
+    console.log(username);
+
+
+    /* this.route.paramMap
+        .subscribe(params => {
+          // tslint:disable-next-line:prefer-const
+          let id = +params.get('id')
+          console.log(id);
+        }); */
   }
 
 }
